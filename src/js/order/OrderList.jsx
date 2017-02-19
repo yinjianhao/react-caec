84135f7432a89c34797edad89658446f84bac32d
yinjianhao <715376377@qq.com>
2017-02-19 22:23:58 +0800

    orderlist

--- src/js/order/OrderList.jsx
+++ src/js/order/OrderList.jsx
@@ -2,11 +2,136 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import className from 'classnames'
+import mobiscroll from "mobiscroll"
 
 import "./orderList.less"
 import ContainerWithHeader from '../../common/component/header/ContainerWithHeader'
 import loading from '../../common/component/loading/loading'
 import { Tabs, TabPane } from '../../common/component/tabs/Tabs'
+
+@withRouter
+class OrderItem extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    status2Type = (status) => {
+        const status2Type = {
+            '01': '待支付',
+            '02': '已支付',
+            '03': '生产中',
+            '04': '生产已完成',
+            '06': '已出库',
+            '07': '待提车',
+            '09': '待审核',
+            '10': '审核未通过',
+            '11': '已取消',
+            '23': '已关闭',
+            '26': '交易成功',
+            '30': '已发货',
+        }
+
+        return status2Type[status];
+    }
+
+    status2Color = (status) => {
+        const status2Color = {
+            '01': 'status-color3',
+            '02': 'status-color3',
+            '03': 'status-color3',
+            '04': 'status-color3',
+            '06': 'status-color3',
+            '07': 'status-color3',
+            '09': 'status-color3',
+            '10': 'status-color2',
+            '11': 'status-color2',
+            '23': 'status-color2',
+            '26': 'status-color2',
+            '30': 'status-color3',
+        }
+
+        return status2Color[status];
+    }
+
+    initGoods = (goods) => {
+
+        if (goods.length === 1) {
+            return (
+                <div>
+                    <div className="detail-item border">
+                        <img src={goods[0].img} />
+                    </div>
+                    <div className="margin-left20 h3">{goods[0].prop}</div>
+                </div>
+            )
+        }
+        return goods.map(item => {
+            return (
+                <div className="detail-item border">
+                    <img src={item.img} />
+                </div>
+            )
+        })
+    }
+
+    initBtn = (status) => {
+        if (status == '01') {
+            return (
+                <div>
+                    <div className="btn" onClick={this.handleCancel}>取消订单</div>
+                    <div className="btn btn-primary" onClick={this.handlePay}>去支付</div>
+                </div>
+            )
+        } else if (status == '11' || status == '23' || status == '26') {
+            return (
+                <div className="btn">再次购买</div>
+            )
+        }
+
+        return (
+            <div className="btn" onClick={this.handleCancel}>取消订单</div>
+        )
+    }
+
+    handlePay = () => {
+        this.props.router.push('/pay');
+    }
+
+    handleCancel = () => {
+        const {onCancel} = this.props;
+
+        if (typeof onCancel === 'function') {
+            onCancel();
+        }
+    }
+
+    render() {
+        const {data, data: {subOrders}} = this.props;
+        const subOrder = subOrders[0];
+        const {status, cost, goods} = subOrder;
+
+        return (
+            <li className="order-item">
+                <div className="item-top border-b">
+                    <span className="h4">{data.time}</span>
+                    <span className={`${this.status2Color(status)} h3`}>{this.status2Type(status)}</span>
+                </div>
+                <div className="item-detail border-b">
+                    {this.initGoods(goods)}
+                </div>
+                <div className="item-goods border-b">
+                    <span className="h4">共<span>{goods.length}</span>件商品</span>
+                    <span className="h4">合计:¥<span className="price-original-right h2">{cost}</span></span>
+                    <span className="h4">实付:¥<span className="price-original-right h2">{cost}</span></span>
+                </div>
+                <div className="item-status">
+                    {this.initBtn(status)}
+                </div>
+            </li>
+        )
+    }
+}
 
 @connect(
     state => {
@@ -27,6 +152,14 @@ export default class OrderList extends Component {
         initialSlide: 0,
     }
 
+    selectOptions = [
+        '预算不足',
+        '价格太高,找到更便宜的地方买了',
+        '现在不想买了',
+        '买了其他商品了',
+        '其他'
+    ]
+
     constructor(props) {
         super(props);
 
@@ -44,6 +177,46 @@ export default class OrderList extends Component {
         })
     }
 
+    handleCancel = () => {
+
+        this.refs.select.instance.show();
+    }
+
+    initOrderList = (curIndex) => {
+        const {index} = this.state;
+        const {orderList} = this.props;
+        const orders = orderList[index];
+
+        if (curIndex !== index) return null;
+
+        if (orders.length === 0) {
+            return (
+                <div className="empty">
+                    <i className="icon-order iconfont"></i>
+                    您还没有相关订单
+            </div>
+            )
+        }
+
+        return orders.map((order, index) => {
+            return <OrderItem key={order.subOrders[0].id} data={order} onCancel={this.handleCancel} />
+        })
+    }
+
+    initOption = () => {
+        return this.selectOptions.map((item, index) => {
+            return (
+                <option key={index} value={index} >{item}</option >
+            )
+        })
+    }
+
+    onSet = (event, inst) => {
+        const value = Number(inst.getVal());
+
+        console.log('取消订单', this.selectOptions[value]);
+    }
+
     render() {
         this.options.initialSlide = this.state.index;
 
@@ -51,24 +224,36 @@ export default class OrderList extends Component {
             <ContainerWithHeader className="order-list" title="我的订单" isLoading={this.props.isLoading}>
                 <Tabs options={this.options} onChange={this.handleChange}>
                     <TabPane tab="全部">
-                        全部
+                        {this.initOrderList(0)}
                     </TabPane>
                     <TabPane tab="待付款">
-                        待付款
+                        {this.initOrderList(1)}
                     </TabPane>
                     <TabPane tab="待发货">
-                        待发货
+                        {this.initOrderList(2)}
                     </TabPane>
                     <TabPane tab="待收货">
-                        待收货
+                        {this.initOrderList(3)}
                     </TabPane>
                     <TabPane tab="待评价">
-                        待评价
+                        {this.initOrderList(4)}
                     </TabPane>
                     <TabPane tab="退款/售后">
-                        退款/售后
+                        {this.initOrderList(5)}
                     </TabPane>
                 </Tabs>
+                <div className="select-wrap">
+                    <mobiscroll.Select
+                        ref="select"
+                        className="cancel-select"
+                        theme="mobiscroll"
+                        display="bottom"
+                        value={0}
+                        onSet={this.onSet}
+                    >
+                        {this.initOption()}
+                    </mobiscroll.Select>
+                </div>
             </ContainerWithHeader>
         );
     }
--- src/js/order/order.model.js
+++ src/js/order/order.model.js
@@ -13,17 +13,63 @@ const orderState = [
 ];
 const ORDER_LIST_URL = `${basePath}/member/order/list`;
 
+function processOrderListData(orders) {
+    //遍历主订单
+    for (let i = 0, l = orders.length; i < l; i++) {
+        let order = orders[i];
+        let subOrders = order.subOrders;
+        //未付款和已关闭,合并所有子订单商品
+        if (order.status == '01' || order.status == '23') {
+            for (let j = 1, k = subOrders.length; j < k; j++) {
+                subOrders[0].goods.concat(subOrders[j].goods);
+            }
+            subOrders = subOrders.slice(0, 1);
+            continue;
+        }
+
+        //新建主订单,将子订单拼接起来
+        if (subOrders.length > 1) {
+            let currentI = i;
+
+            for (subOrder of subOrders) {
+                let newOrder = Object.assign(order);
+                newOrder.subOrders = [subOrder];
+
+                //将新增订单按顺序加入列表当中
+                i++;
+                orders.splice(i, 0, newOrder);
+            }
+
+            //删除原有订单
+            orders.splice(currentI, 1);
+            i--;
+        }
+    }
+
+    return orders;
+}
+
 const model = {
     name: 'order',
     initialState: {
         isLoading: false,
-        orderList: []
+        orderList: {
+            0: [],
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+            5: [],
+        }
     },
     reducers: createReducer([], {
         ORDER_LIST(state, action) {
-            const { data } = action.payLoad.response;
+            let {index} = action.payLoad;
+            let { data } = action.payLoad.response;
 
-            return {...state, orderList: data, isLoading: false }
+            let orderList = processOrderListData(data);
+
+            return { ...state, orderList: { ...state.orderList, [index]: orderList }, isLoading: false }
         }
     }),
     sagas: {
@@ -36,11 +82,12 @@ const model = {
                 type: baseType,
                 payLoad: {
                     successType: 'ORDER_LIST',
+                    successPayLoad: { index },
                     url: ORDER_LIST_URL,
                     type: 'GET',
                     params: {
                         pageIndex: 1,
-                        pageSize: 20,
+                        pageSize: 30,
                         status: JSON.stringify(orderState[index])
                     }
                 }
